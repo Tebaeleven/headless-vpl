56725ab025c0eb31765db52fb7698fa7075ff4e4
Tebaeleven <73474645+Tebaeleven@users.noreply.github.com>
2024-11-16 16:06:22 +0900

    ルーティング追加

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import { Link, redirect, useNavigate } from "@/router";
+import { Button } from "@/components/ui/button";
+
+function index() {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          navigate("/example");
+        }}
+      >
+        Example
+      </Button>
+    </div>
+  );
+}
+
+export default index;
